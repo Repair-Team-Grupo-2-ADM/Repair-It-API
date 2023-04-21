64b7ef62644c3af774b80005827c7f7d8c957d17
Edgar Andre Anco Copaja <u201724411@upc.edu.pe>
2023-04-21 16:42:50 -0500

    update device class

--- src/main/java/com/repair_team/repair_it_api/entities/Device.java
+++ src/main/java/com/repair_team/repair_it_api/entities/Device.java
@@ -1,5 +1,30 @@
 package com.repair_team.repair_it_api.entities;
 
-public class Device {
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+
+@Entity
+@Table(name = "devices")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Device implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "customer_id", nullable = false)
+    @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+    private Customer customer;
+    @Column(name = "model", nullable = false, length = 15)
+    private String model;
+    @Column(name = "brand", nullable = false, length = 15)
+    private String brand;
+    @Column(name = "status", nullable = false)
+    private Boolean status;
 }
