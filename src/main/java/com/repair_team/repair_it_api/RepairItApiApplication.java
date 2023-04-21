639fb2ece223ad6e065ee1f2f706d441e7ba342c
Edgar Andre Anco Copaja <u201724411@upc.edu.pe>
2023-04-21 14:43:44 -0500

    start spring project

---
+++ src/main/java/com/repair_team/repair_it_api/RepairItApiApplication.java
@@ -0,0 +1,13 @@
+package com.repair_team.repair_it_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepairItApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RepairItApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/repair_team/repair_it_api/RepairItApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.repair_team.repair_it_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepairItApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
